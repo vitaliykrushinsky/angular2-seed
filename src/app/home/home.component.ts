c4df81a0abd4b9c862b20dbb963e96af5a16d544
Vitaliy Krushinsky <vitaliykrushinsky@gmail.com>
2016-12-30 11:02:01 +0200

    set up app's form

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
-import {Component} from '@angular/core';
+import { Employee } from './../models/employee.model';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'home',
@@ -6,4 +7,14 @@ import {Component} from '@angular/core';
   templateUrl: './home.component.html'
 })
 export class HomeComponent {
+  languages: string[] = ['English', 'Spanish', 'Others'];
+  model = new Employee('John', 'Doe', true, "w2", 'English');
+
+  firstLetterToUpperCase(value: string): void {
+    if (value.length > 0) {
+      this.model.firstName = value.charAt(0).toUpperCase() + value.slice(1);
+    } else {
+      this.model.firstName = value;
+    }
+  }
 }
